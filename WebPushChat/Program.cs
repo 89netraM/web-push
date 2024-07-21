70f54876a8f25a92acb74d30439dba3075495c9c
Mårten Åsberg <marten.asberg@outlook.com>
2024-07-21 13:13:47 +0200

    Init .NET Client Chat app

---
+++ WebPushChat/Program.cs
@@ -0,0 +1,13 @@
+using System.Net.Http;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using WebPushChat;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
